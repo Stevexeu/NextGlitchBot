90fa08610fbe5fc67f065f9e42304511c5b99f30
solaresports <70237441+solaresports@users.noreply.github.com>
2021-02-10 05:43:20 +0100

    test
    test123

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,14 +1,14 @@
 module.exports = client => {
-    console.log("[Unreal] De bot is aan het opstarten");
-    console.log("[Unreal] De data is ingeladen");
-    console.log("[Unreal] Plugin date: 5%");
-    console.log("[Unreal] Plugin date: 19%");
-    console.log("[Unreal] Plugin date: 26%");
-    console.log("[Unreal] Plugin date: 39%");
-    console.log("[Unreal] Plugin date: 51%");
-    console.log("[Unreal] Plugin date: 78%");
-    console.log("[Unreal] Plugin date: 92%");
-    console.log("[Unreal] Plugin date: 100%");
-    console.log("[Unreal] De bot is volledig opgestart");
+    console.log("[NEXTGLITCH] De bot is aan het opstarten");
+    console.log("[NEXTGLITCH] De data is ingeladen");
+    console.log("[NEXTGLITCH] Plugin date: 5%");
+    console.log("[NEXTGLITCH] Plugin date: 19%");
+    console.log("[NEXTGLITCH] Plugin date: 26%");
+    console.log("[NEXTGLITCH] Plugin date: 39%");
+    console.log("[NEXTGLITCH] Plugin date: 51%");
+    console.log("[NEXTGLITCH] Plugin date: 78%");
+    console.log("[NEXTGLITCH] Plugin date: 92%");
+    console.log("[NEXTGLITCH] Plugin date: 100%");
+    console.log("[NEXTGLITCH] De bot is volledig opgestart");
 
 }
