9fd3b401647162681bdf4d53b659f95698bc23aa
[NG]Stevex <70237441+steftijssen98@users.noreply.github.com>
2021-02-10 06:29:48 +0100

    u

--- commands/teams/testt.js
+++ commands/teams/testt.js
@@ -1,25 +1,25 @@
 const discord = require("discord.js");
 module.exports = {
-    name: 'specops1',
+    name: 'testt',
     category: 'teams',
-    description: 'specops1',
+    description: 'testt',
     run: async (bot, message, args, guild) => {
 
         var botIcon = bot.user.displayAvatarURL;
 
         var botEmbed = new discord.MessageEmbed()
-            .setTitle("[NEXT SPECOPS]")
+            .setTitle("[NEXT SOLDIERS]")
             .setDescription(`    
             **Game:** League of Legends
             **Type:** Competitive
             **Organization:** NextGlitch - eSports
-            **Team Name:** Next Specops
+            **Team Name:** Next Soldiers
             
             If you are intrested in this team name.
             Then please click on the ✅ emoji to vote.
             `)
-            .setThumbnail("https://cdn.discordapp.com/attachments/766797739609817110/808004894279335956/RangeRovers.PNG")
-            .setColor("#5a85f2")
+            .setThumbnail("https://cdn.discordapp.com/attachments/721405053893607484/808929197237207040/guardians.png")
+            .setColor("#e08077")
 
         message.delete().catch(err => console.log(err));
         return message.channel.send(botEmbed);
